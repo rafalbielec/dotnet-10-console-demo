810810644d3e657835e339d62279514d94147881
Rafal Bielec <info@rafalbielec.com>
2026-09-12 15:22:04 +0200

    expand example

--- Examples/FieldPropertyExample.cs
+++ Examples/FieldPropertyExample.cs
@@ -6,11 +6,21 @@ namespace Examples;
 
 public class FieldPropertyExample : IExample
 {
+    private class Model
+    {
+        public int Number { get; set; }
+    }
+
     public Task RunExampleAsync()
     {
         Name = " Raf  ";
         Console.WriteLineInformation($"{nameof(Name)} is {Name}.");
         Console.WriteLineInformation($"{nameof(SelfAssigned)} is {SelfAssigned}.");
+
+        // Instance assignment without checking for null first
+        Model m = null;
+        m?.Number = 100;
+        m?.Number += 100;
 
         return Task.CompletedTask;
     }
