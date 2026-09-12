dd768bbb4498d14d344544d2a72dba9a29a7a2d0
Rafal Bielec <info@rafalbielec.com>
2026-09-12 16:26:30 +0200

    add constructor and assigmment examples

---
+++ Examples/CompoundAssignmentExample.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Threading.Tasks;
+using Abstractions;
+
+namespace Examples;
+
+public class CompoundAssignmentExample : IExample
+{
+    private struct Money
+    {
+        public decimal Value { get; }
+
+        public Money(decimal value)
+        {
+            Value = value;
+        }
+
+        public static Money operator +(Money a, Money b) => new(a.Value + b.Value);
+        public static Money operator ++(Money value) => new(value.Value + 1);
+
+        public override string ToString()
+        {
+            return $"{Value:0.00}";
+        }
+    }
+
+    public Task RunExampleAsync()
+    {
+        Money total = new(100);
+        total += new Money(25);
+        total++;
+
+        Console.WriteLineInformation($"Amount in total is {total}.");
+
+        return Task.CompletedTask;
+    }
+}
---
+++ Examples/ConstructorExample.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Threading.Tasks;
+using Abstractions;
+
+namespace Exaples;
+
+public partial class ConstructorExample
+{
+    public partial ConstructorExample();
+}
+
+public partial class ConstructorExample : IExample
+{
+    public partial ConstructorExample() { }
+
+    public Task RunExampleAsync()
+    {
+        var self = new ConstructorExample();
+        Console.WriteLineInformation($"{nameof(ConstructorExample)} is not null: {self is not null}.");
+
+        return Task.CompletedTask;
+    }
+}
